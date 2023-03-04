f15e5a2019d03f0f35cbe5455919f6876d5fe0ae
Joseph Collard <jcollard@users.noreply.github.com>
2023-03-04 04:01:29 -0700

    refactor: Refactor ShapelessRecipe to be a record.

--- Solution/CaptainCoder.CraftingSystem/ShapelessRecipe.cs
+++ Solution/CaptainCoder.CraftingSystem/ShapelessRecipe.cs
@@ -1,39 +1,5 @@
-public class ShapelessRecipe<T> : IShapelessRecipe<T>
+public record ShapelessRecipe<T>(IEnumerable<T> Ingredients, ICraftingCategory Category, IEnumerable<T> Result) : IShapelessRecipe<T>
 {
-    private readonly List<T> _ingredients;
-    private readonly List<T> _result;
-    public ShapelessRecipe(IEnumerable<T> ingredients, ICraftingCategory category, IEnumerable<T> result)
-    {
-        if (ingredients.Count() < 1) { throw new ArgumentException("Must have at least 1 ingredient."); }
-        if (result.Count() < 1) { throw new ArgumentException("Must have at least 1 result item."); }
-        _ingredients = ingredients.ToList();
-        Category = category;
-        _result = result.ToList();
-    }
-
-    /// <summary>
-    /// Returns an IEnumerable of the items necessary
-    /// to perform this recipe. The order is undefined.
-    /// </summary>
-    public IEnumerable<T> Ingredients
-    {
-        get
-        {
-            foreach (T ingredient in _ingredients)
-            {
-                yield return ingredient;
-            }
-        }
-    }
-    public IEnumerable<T> Result
-    {
-        get
-        {
-            foreach (T result in _result)
-            {
-                yield return result;
-            }
-        }
-    }
-    public ICraftingCategory Category { get; }
+    public IEnumerable<T> Ingredients { get; init; } = Ingredients.AsEnumerable(); 
+    public IEnumerable<T> Result { get; init; } = Result.AsEnumerable();
 }
